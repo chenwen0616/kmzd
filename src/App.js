5c2cb1f7d5f815eaa3f4c8cab37a68b239663244
chenwen0616 <951867067@qq.com>
2021-09-30 14:31:49 +0800

    update headTitle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col, ButtonToolbar, Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              {/* <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo"/>
+              </header> */}
+              <Container>
+                  <Row className="show-grid">
+                      <Col xs={12} md={4}>
+                          <p className="App-intro">
+                              Buttons Demo
+                          </p>
+                      </Col>
+                      <Col xs={6} md={8}>
+                          <ButtonToolbar>
+                              {/* Standard button */}
+                              <Button>Default</Button>
+                              <Button variant="warning">Primary</Button>
+                              <Button variant="success">Success</Button>
+                              <Button variant="info">Info</Button>
+                          </ButtonToolbar>
+                      </Col>
+                  </Row>
+              </Container>
+          </div>
+      );
+  }
 }
 
 export default App;
